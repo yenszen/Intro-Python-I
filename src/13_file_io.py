8502f1a18a6278037aaacafc1321d8f33daea690
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-07-13 11:29:49 +0800

    completed tasks 12 & 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open("foo.txt", "r")
+print(f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f1 = open("bar.txt", "w")
+f1.write("Hello world \n")
+f1.write("This is a new file \n")
+f1.write("Titled bar.txt")
+f1.close()
